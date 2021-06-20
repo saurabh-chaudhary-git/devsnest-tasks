d43d6fd178b87d9529f02750238d594709c16a07
saurabh-chaudhary-git <chaudharysaurabh279@mail.com>
2021-06-20 22:15:17 +0530

    Day 8

---
+++ Day 8/script.js
@@ -0,0 +1,26 @@
+let bookedSeatsElement = document.querySelector(`.booking_seats`);
+let remainingSeatsElement = document.querySelector(`.remaining_seats`);
+let seats = document.querySelectorAll(`.seats`);
+
+let bookedSeats = 0;
+let reaminingSeats = seats.length;
+
+bookedSeatsElement.innerText = bookedSeats;
+remainingSeatsElement.innerText = reaminingSeats;
+
+seats.forEach((seat) => {
+  seat.addEventListener(`click`, () => {
+    // console.log(`you clicked`);
+    seat.classList.toggle(`booked`);
+    seat.classList.contains(`booked`) ? bookeSeats(1) : bookeSeats(-1);
+  });
+});
+
+const unBookSeats = () => {
+  bookedSeats -= 1;
+  reaminingSeats += 1;
+
+  bookedSeatsElement.innerText = bookedSeats;
+  remainingSeatsElement.innerText = reaminingSeats;
+};
+const newElem = document.createElement(`div`);
